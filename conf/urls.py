a93e56bda3763273fc77e1f8bfa0e8ba6bc23663
davidmeel <rahmatjonov2707@gmail.com>
2023-12-22 10:13:54 +0500

    first commit

--- conf/urls.py
+++ conf/urls.py
@@ -23,7 +23,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include("pages.urls"))
+    path('', include("pages.urls"))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
